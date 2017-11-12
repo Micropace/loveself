f62b235b1309f5ca977b34ce1e4b925c7dd72195
suffrajet <suffrajets@hotmail.com>
2017-11-12 18:08:23 +0800

    增加页面颜色的配置

--- backend/src/main/java/com/weibu/loveself/entity/QuestionConfig.java
+++ backend/src/main/java/com/weibu/loveself/entity/QuestionConfig.java
@@ -9,11 +9,32 @@ import javax.persistence.Column;
 /**
  * 题库的配置信息表
  * 一套题目首页图片和文字、结束页图片和文字、banner等的配置信息
+ *
+ * Bar颜色：barColor
+ * Bar文字颜色：barTextColor
+ * 页面背景色：backgroundColor
+ * 题干背景色：questionTitleColor
+ * 题目选项背景色：questionSectionColor
+ 题目选项选中背景色：questionSectionSelectedColor
+ 结束页文字颜色：endPageTextColor
  */
 public class QuestionConfig extends MybatisBasePagerEntity {
     /** 题库ID */
     @Column(name = "id_question", nullable = false)
     private Long idQuestion;
+    /** 页面背景色 */
+    @Column(name = "background_color")
+    private String backgroundColor;
+    /** 页面描述文字颜色 */
+    @Column(name = "page_discription_color")
+    private String pageDiscriptionColor;
+    /** 题干、题目选项选中背景色 */
+    @Column(name = "question_active_color")
+    private String questionActiveColor;
+    /** 题目选项背景色 */
+    @Column(name = "question_option_color")
+    private String questionOptionColor;
+    /** Bar标题文字 */
     @Column(name = "bar_title_text")
     private String barTitleText;
     /** banner文字 */
@@ -45,6 +66,10 @@ public class QuestionConfig extends MybatisBasePagerEntity {
     public String toString() {
         return "QuestionConfig{" +
                 "idQuestion=" + idQuestion +
+                ", backgroundColor='" + backgroundColor + '\'' +
+                ", pageDiscriptionColor='" + pageDiscriptionColor + '\'' +
+                ", questionActiveColor='" + questionActiveColor + '\'' +
+                ", questionOptionColor='" + questionOptionColor + '\'' +
                 ", barTitleText='" + barTitleText + '\'' +
                 ", bannerText='" + bannerText + '\'' +
                 ", bannerPicName='" + bannerPicName + '\'' +
@@ -63,6 +88,38 @@ public class QuestionConfig extends MybatisBasePagerEntity {
 
     public void setIdQuestion(Long idQuestion) {
         this.idQuestion = idQuestion;
+    }
+
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public String getPageDiscriptionColor() {
+        return pageDiscriptionColor;
+    }
+
+    public void setPageDiscriptionColor(String pageDiscriptionColor) {
+        this.pageDiscriptionColor = pageDiscriptionColor;
+    }
+
+    public String getQuestionActiveColor() {
+        return questionActiveColor;
+    }
+
+    public void setQuestionActiveColor(String questionActiveColor) {
+        this.questionActiveColor = questionActiveColor;
+    }
+
+    public String getQuestionOptionColor() {
+        return questionOptionColor;
+    }
+
+    public void setQuestionOptionColor(String questionOptionColor) {
+        this.questionOptionColor = questionOptionColor;
     }
 
     public String getBarTitleText() {
